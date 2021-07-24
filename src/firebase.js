c46d27ef28f1ecddde39554082e6f066c72fa690
varun <vbop1711@gmail.com>
2021-07-25 02:21:50 +0400

    updated firebase config to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBom4zDke9mYimbaK5pacCXKwvITxGkluQ",
-  authDomain: "e-commerce-store-37b6c.firebaseapp.com",
-  databaseURL: "https://e-commerce-store-37b6c-default-rtdb.firebaseio.com",
-  projectId: "e-commerce-store-37b6c",
-  storageBucket: "e-commerce-store-37b6c.appspot.com",
-  messagingSenderId: "726421179450",
-  appId: "1:726421179450:web:52f4f76b9bf4c11104f903",
-  measurementId: "G-PTYZ6ND9BV",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
